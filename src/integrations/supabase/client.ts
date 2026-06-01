58cd920c2a0977527af6e635b4b2225d69940c5b
AlexCrowd3 <165948145+AlexCrowd3@users.noreply.github.com>
2026-06-01 21:28:40 +0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = "https://iqzuhtxvpuxfxltiyfom.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxenVodHh2cHV4ZnhsdGl5Zm9tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0MTM3MjgsImV4cCI6MjA2Nzk4OTcyOH0.xOSkVkViX8jcFZJLmpTYqAGEu6x44UOX-ykhTgtSiRU";
+const SUPABASE_URL = "";
+const SUPABASE_PUBLISHABLE_KEY = "";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
